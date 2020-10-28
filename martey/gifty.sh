f3f9a436f6f70d27191dd3ce22d5f961979e8f3c
root <root@smartechcareers.us-central1-a.c.pure-object-286019.internal>
2020-10-28 15:06:57 +0000

    first commit adding george.sh felix.sh. and gifty.sh >

---
+++ martey/gifty.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+# Demostrate the use of shift and while loops.
+# Display the first three parameters.
+echo "Parameter 1: ${1}"
+echo "Parameter 1: ${2}"
+echo "Parameter 1: ${3}"
+echo 
